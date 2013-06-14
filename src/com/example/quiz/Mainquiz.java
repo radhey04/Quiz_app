3cbc6e888618fa1646b4fa2c9025ca6f1bd27329
bazookan <aby.vinod@gmail.com>
2013-06-14 22:19:11 +0530

    Quiz working. After the quiz is over, currently we are going back to the Admin_activity. Need to go to a publishing page.
    Signed-off-by: bazookan <aby.vinod@gmail.com>

--- src/com/example/quiz/Mainquiz.java
+++ src/com/example/quiz/Mainquiz.java
@@ -18,50 +18,102 @@ public class Mainquiz extends Activity {
 
 	Context context = this;
 	
+	
+	private Integer qno;
+    private String answ;
+    private Integer totq;
+    private String timeleft;
+    CheckBox chka;
+	CheckBox chkb;
+	CheckBox chkc;
+	CheckBox chkd;
+	TextView Q;
+    TextView opta;
+    TextView optb;
+    TextView optc;
+    TextView optd;
+    MyDBAdapter ad;
+    
+    private void updateactivity()
+    {    	
+	    Cursor c1=ad.getQno(qno);
+	    Q.setText(c1.getString(1));
+	    opta.setText(c1.getString(2));
+	    optb.setText(c1.getString(3));
+	    optc.setText(c1.getString(4));
+	    optd.setText(c1.getString(5));
+	    answ=c1.getString(6).toString();
+	    chka.setChecked(false);
+	    chkb.setChecked(false);
+	    chkc.setChecked(false);
+	    chkd.setChecked(false);
+	    Log.d("Debug","Updated");	    
+    }
+    
+    private void Initialize()
+    {
+    	Q=(TextView) findViewById(R.id.textView1);
+	    opta=(TextView) findViewById(R.id.textView2);
+	    optb=(TextView) findViewById(R.id.textView3);
+	    optc=(TextView) findViewById(R.id.textView4);
+	    optd=(TextView) findViewById(R.id.textView5);
+	    chka = (CheckBox)findViewById(R.id.checkBox1);
+		chkb = (CheckBox)findViewById(R.id.checkBox2);
+		chkc = (CheckBox)findViewById(R.id.checkBox3);
+		chkd = (CheckBox)findViewById(R.id.checkBox4);
+	    ad=new MyDBAdapter(context);
+
+	    qno=1;											//Initialise the question number
+	    answ="";										//Initialise the answ string
+	    
+    }
+    
+    private Boolean isquizover()
+    {
+    	if(qno>totq)
+    	{
+    		Log.d("Debug","Quiz Over.");
+    		return true;
+    	}
+    	else if(timeleft.equals(""))
+    	{
+    		Log.d("Debug","Time up.");
+    		return true;
+    	}
+    	else
+    	{
+    		Log.d("Debug","Continue to next question");
+    		return false;
+    	}
+    }
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_mainquiz);
 		
-		final MyDBAdapter ad=new MyDBAdapter(context);
-		
 		Bundle b = getIntent().getExtras();
-		final Integer qno=b.getInt("qno");
-		final Integer totq=b.getInt("totq");
-		final String timeleft=b.getString("timeleft");
 		
-		Log.d("Debug","The current values");
-		Log.d("Debug",qno.toString());
+		totq=b.getInt("totq");
+		timeleft=b.getString("timeleft");
+		
+		Log.d("Debug","The total number of questions");
 		Log.d("Debug",totq.toString());
+		Log.d("Debug","Time");
 		Log.d("Debug",timeleft);
 	    
-		TextView t1=(TextView) findViewById(R.id.textView1);
-	    TextView t2=(TextView) findViewById(R.id.textView2);
-	    TextView t3=(TextView) findViewById(R.id.textView3);
-	    TextView t4=(TextView) findViewById(R.id.textView4);
-	    TextView t5=(TextView) findViewById(R.id.textView5);
-	    Button submit=(Button) findViewById(R.id.button1);
+		Button submit=(Button) findViewById(R.id.button1);
 	    
-	    final Cursor c1=ad.getQno(qno);
-	    t1.setText(c1.getString(1));
-	    t2.setText(c1.getString(2));
-	    t3.setText(c1.getString(3));
-	    t4.setText(c1.getString(4));
-	    t5.setText(c1.getString(5));
-	    final String ans=c1.getString(6).toString();
+	    Initialize();
+	    updateactivity();
 	    
-	    Log.d("Debug","Waiting for a new option");
 	    submit.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
 			    
-			    final CheckBox chka = (CheckBox)findViewById(R.id.checkBox1);
-				final CheckBox chkb = (CheckBox)findViewById(R.id.checkBox2);
-				final CheckBox chkc = (CheckBox)findViewById(R.id.checkBox3);
-				final CheckBox chkd = (CheckBox)findViewById(R.id.checkBox4);
-				String option = "";
+			    String option = "";
 			    if(chka.isChecked())
 				{
 					option=option.concat("A");
@@ -79,41 +131,44 @@ public class Mainquiz extends Activity {
 					option=option.concat("D");
 				}
 		        //Inserts a String value into the mapping of this Bundle
+				Log.d("Debug","Your answer");
 				Log.d("Debug",option);
+				Log.d("Debug","Correct answer");
+				Log.d("Debug",answ);
 			    if(!option.equals(""))
 			    {
-			    	Bundle bn=new Bundle();
-			    	Integer qnon=qno+1;
-		        	if(option.equals(ans))	// Always the order will be A-B-C-D. :)
+			    	if(option.equals(answ))	// Always the order will be A-B-C-D. :)
 		        	{
-		        		Toast.makeText(getApplicationContext(), "You are correct", Toast.LENGTH_SHORT).show();		        				        		
+		        		Toast.makeText(getApplicationContext(), "You are correct", Toast.LENGTH_SHORT).show();
+		        		Log.d("Debug","Correct");
 		        	}
 		        	else 
 		        	{		        		
 		        		Toast.makeText(getApplicationContext(), "You are wrong", Toast.LENGTH_SHORT).show();
+		        		Log.d("Debug","Incorrect");
 		        	}
-		        	bn.putInt("qno",qnon);
-			        bn.putInt("totq",totq);
-			        bn.putString("time",timeleft);
-			        //Intent i= new Intent(Mainquiz.this,Mainquiz.class);
-			        // Solution given above: 
-			        Intent i = new Intent(getApplicationContext(), Mainquiz.class);
-			        //Add the bundle to the intent.
-			        i.putExtras(bn);
-			        startActivity(i);
-					finish();			        
+			    	qno=qno+1;
+			    	if(!isquizover())
+			    	{
+			    		updateactivity();
+			    		Log.d("Debug","Question Answered");
+			    	}
+			    	else
+			    	{
+			    		Toast.makeText(getApplicationContext(), "Congrats", Toast.LENGTH_SHORT).show();
+		        		Log.d("Debug","Quiz Over! Congrats");
+		        		Intent i=new Intent(getApplicationContext(), Admin.class);
+						startActivity(i);
+						finish();
+			    	}
+			    	
 			    }
 				else
 				{
 					Toast.makeText(getApplicationContext(), "You forgot to enter answer.", Toast.LENGTH_SHORT).show();
 				}
-			    Log.d("Debug","Question Answered");
-			    
 			}
 		});
-	    
-	    
-
 	}
 
 	@Override
