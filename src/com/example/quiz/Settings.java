32b2cf6aab11cba985d3ebbaa1fbd601797dabc9
bazookan <aby.vinod@gmail.com>
2013-06-15 19:56:51 +0530

    Settings page modified. Auto suggest the name for kutti corrections. :) Else, show hint in a background favourable way.
    Signed-off-by: bazookan <aby.vinod@gmail.com>

--- src/com/example/quiz/Settings.java
+++ src/com/example/quiz/Settings.java
@@ -3,7 +3,6 @@ package com.example.quiz;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -18,29 +17,46 @@ public class Settings extends Activity {
 	private Integer Timer = 0;
 	String Name="";
 	String ID="";
-	
+	SettingsDBAdapter set;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
 		
+		set = new SettingsDBAdapter(context);
+		
 		final EditText name = (EditText) findViewById(R.id.editText1);
 		final EditText id = (EditText) findViewById(R.id.editText2);
 		final CheckBox timer = (CheckBox) findViewById(R.id.checkBox1);
 		Button sub = (Button) findViewById(R.id.button1);
+		set.dropset();
+		set.updatemem();
+		if(set.Name.equals(""))
+		{
+			name.setHint("Your Name");
+		}
+		else
+		{
+			name.setText(set.Name);
+		}
+		if(set.ID.equals(""))
+		{
+			id.setHint("Your ID");
+		}            
+		else
+		{
+			id.setText(set.ID);
+		}
 		//Shifted the commented section here below
 		
 		sub.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
-				SettingsDBAdapter set = new SettingsDBAdapter(context);
+				
 				
 				Name = name.getText().toString();
 				ID = id.getText().toString();
-				Log.d("Debug",Name);
-				Log.d("Debug",ID);
-				Log.d("Debug","Before pressing");
 				if(timer.isChecked()) {
 					Timer = 1;
 				}
@@ -52,7 +68,7 @@ public class Settings extends Activity {
 			}
 		});
 		
-		
+//		
 	}
 
 	@Override
