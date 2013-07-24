cd44a3ab85762cd4c8676c231385dd7c6b942105
Nikhil <vvnikhil@gmail.com>
2013-07-25 01:21:10 +0530

    Forgot to add the additional PHP Codes...

---
+++ PHP_Codes/extend.php
@@ -0,0 +1,26 @@
+<?php
+	$con=mysqli_connect("localhost","root","","quiz");
+	$Quiz_Name = "'".$_POST["QuizName"]."'";
+	$Deadline = $_POST["Deadline"];
+	$timestamp = strtotime($Deadline);
+	$Deads = date('YmdHis', $timestamp);
+	$Deads = $Deads+235959;
+	$StartTime = date("YmdHis");
+	if($StartTime<$Deads)
+	{
+		if($Quiz_Name != "''")
+		{
+			mysqli_query($con,"UPDATE `quiz`.`tests` SET Deadline=$Deads WHERE Quiz_Name=$Quiz_Name");
+			echo $Quiz_Name." has been extended to ".$Deads;
+		}
+		else
+		{
+			echo "The field Quiz name cannot be empty.";
+		}
+	}
+	else
+	{
+		echo "Please give a future date/ Use the Stop Test option.";
+	}
+
+?>
