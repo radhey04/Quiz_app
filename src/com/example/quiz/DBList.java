ccc49e4eab7e2dfb104cf4b78173eba80f5f7524
Nikhil <vvnikhil@gmail.com>
2013-07-28 20:46:44 +0530

    Merged the user DBList and master DBList

--- src/com/example/quiz/DBList.java
+++ src/com/example/quiz/DBList.java
@@ -1,12 +1,14 @@
 package com.example.quiz;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
+import java.io.OutputStream;
 import java.net.URL;
+import java.net.URLConnection;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -20,7 +22,9 @@ import org.apache.http.params.HttpParams;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.util.Log;
@@ -40,6 +44,8 @@ public class DBList extends ListActivity {
 	SettingsDBAdapter set;
 	String url;
 	int i=0;
+	public static final int progress_bar_type = 0; 
+	private ProgressDialog pDialog;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -52,20 +58,74 @@ public class DBList extends ListActivity {
 		set.updatemem();
 		url = set.URL+"listdir.php";
 		Log.d("debug", url);
-		task.execute(url);		
+		task.execute(url);	
+		Toast.makeText(context, "Connecting to the server. Timeout is 20 seconds.", Toast.LENGTH_SHORT).show();
 	}
-	private class DownloadWebPage extends AsyncTask<String, Void, String> {
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+			case progress_bar_type: // we set this to 0
+				pDialog = new ProgressDialog(this);
+				pDialog.setMessage("Downloading file. Please wait...");
+				pDialog.setIndeterminate(false);
+				pDialog.setMax(100);
+				pDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+				pDialog.setCancelable(true);
+				pDialog.show();
+				return pDialog;
+			default:
+				return null;
+		}
+	}
+	private class DownloadWebPage extends AsyncTask<String, String, String> {
+		@Override
+		 protected void onPreExecute() {
+		  super.onPreExecute();
+		  showDialog(progress_bar_type);
+		}
 		@Override
 		protected String doInBackground(String... urls) {
 			Log.d("DEBUG", "Inside dwp");
 			String response = "";
+			int count;
 			for (String url : urls) {
 				try {
 					String path = db.chkdir();
 					File root = new File(path);
 			        Log.d("DEBUG", "ROOT"+root);
 			        URL u = new URL(url);
-					HttpURLConnection c = (HttpURLConnection) u.openConnection();
+			        URLConnection conection = u.openConnection();
+		            conection.connect();
+		            // getting file length
+		            int lenghtOfFile = conection.getContentLength();
+		            Log.d("DEBUG", ""+lenghtOfFile);
+		            // input stream to read file - with 8k buffer
+		            InputStream input = new BufferedInputStream(u.openStream(), 8192);
+		            
+		            // Output stream to write file
+		            OutputStream output = new FileOutputStream(new File(root, pressed));
+		            
+		            byte data[] = new byte[1024];
+		 
+		            long total = 0;
+		 
+		            while ((count = input.read(data)) != -1) {
+		                total += count;
+		                // publishing the progress....
+		                // After this onProgressUpdate will be called
+		                Log.d("DEBUG", ""+(int)((total*100)/lenghtOfFile));
+		                publishProgress(""+(int)((total*100)/lenghtOfFile));
+		                // writing data to file
+		                output.write(data, 0, count);
+		            }
+		 
+		            // flushing output
+		            output.flush();
+		 
+		            // closing streams
+		            output.close();
+		            input.close();
+					/*
+			        HttpURLConnection c = (HttpURLConnection) u.openConnection();
 			        c.setRequestMethod("GET");
 			        c.setDoOutput(true);
 			        c.connect();
@@ -80,19 +140,30 @@ public class DBList extends ListActivity {
 			            //publishProgress("" + (int)((total*100)/lenghtOfFile));
 			            f.write(buffer, 0, len1);
 			        }
-			        f.close();
+			        f.close();*/
 			        Log.d("DEBUG", "Importing "+path+"/"+pressed);
 			        db.importDB("Quiz",path+"/"+pressed);
 				} catch (Exception e) {
 					e.printStackTrace();
 					Log.d("DEBUG", "Exception");
 				}
+				dismissDialog(progress_bar_type);
 				finish();
 			}
 			Log.d("DEBUG", "Done dwp");
 			return response;
 	}
-	
+		protected void onProgressUpdate(String... progress) {
+			// setting progress percentage
+			Log.d("Debug", "Done:"+progress[0]);
+			if(progress[0] != "0");
+			{
+				pDialog.setProgress(Integer.parseInt(progress[0]));
+			}
+					
+		//pDialog.setProgress(Integer.parseInt(progress[0]));
+		
+		}
 	}
 
 	private class DownloadWebPageTask extends AsyncTask<String, Void, String> {
@@ -103,7 +174,7 @@ public class DBList extends ListActivity {
 			for (String url : urls) {
 				HttpGet httpGet = new HttpGet(url);
 				HttpParams httpParameters = new BasicHttpParams();
-				int timeoutConnection = 3000;
+				int timeoutConnection = 20000;
 				HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
 				int timeoutSocket = 5000;
 				HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
@@ -145,7 +216,12 @@ public class DBList extends ListActivity {
 				Toast.makeText(context,  "Connection to "+url+" could not be established" , Toast.LENGTH_LONG).show();
 				finish();
 			}
-			
+			else if (result.equals(""))
+			{
+				Toast.makeText(context, "Wrong server", Toast.LENGTH_LONG).show();
+				finish();
+			}
+				
 			Log.d("DEBUG", result);
 			String[] FRUITS = result.split("#");
 			
