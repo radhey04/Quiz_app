5cec3ff9bd86473e6fa4ee708864c167bed5d65d
Nikhil <vvnikhil@gmail.com>
2013-08-26 23:24:44 +0530

    The login credentials are checked and the password generation is implemented.

--- src/com/example/quiz/Settings.java
+++ src/com/example/quiz/Settings.java
@@ -1,11 +1,21 @@
 package com.example.quiz;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -74,7 +84,7 @@ public class Settings extends Activity {
 		}
 		if(set.URL.equals(""))
 		{
-			url.setText("http://students.iitm.ac.in/mocktest/");
+			url.setText("http://192.168.1.10/app/");
 		}
 		else
 		{
@@ -112,6 +122,11 @@ public class Settings extends Activity {
 			    	builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
 			    	    public void onClick(DialogInterface dialog, int which) {
 							//TODO:Nikhil's authenticate request code comes here
+			    	    	DownloadWebPageTask task = new DownloadWebPageTask();
+			    	    	String Student_ID = "RollNumber='"+id.getText().toString()+"'";
+							String Name = "Name='"+name.getText().toString()+"'";
+							String url = "http://192.168.1.10/app/registration.php?"+Student_ID+"&"+Name;
+							task.execute(url);
 			    	    }
 			    	});
 			    	builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
@@ -156,11 +171,12 @@ public class Settings extends Activity {
 					// The settings db is now consistent with the master code..dh is always zero
 					// TODO: Authenticate the password here and call below functions only if 
 					// successful authentication
-					set.updateset(Name, ID, Timer,dh,URL,Pass);
-					Toast.makeText(getApplicationContext(), "Settings updated", Toast.LENGTH_SHORT).show();
-					Intent i=new Intent(getApplicationContext(), Admin.class);
-					startActivity(i);
-					finish();
+					DownloadWebPageTask1 task1 = new DownloadWebPageTask1();
+	    	    	String Student_ID = "RollNumber='"+ID+"'";
+					String UserName = "Name='"+Name+"'";
+					String Password = "Password='"+Pass+"'";
+					String url = "http://192.168.1.10/mocktest/UserAuthenticate.php?"+Student_ID+"&"+UserName+"&"+Password;
+					task1.execute(url);
 				}
 			}
 		});
@@ -185,5 +201,103 @@ public class Settings extends Activity {
 		else if(url.contains(")"))
 			validity=false;
 		return validity;
+	}
+	private class DownloadWebPageTask extends AsyncTask<String, Void, String> {
+		@Override
+		protected String doInBackground(String... urls) {
+			Log.d("DEBUG", "Inside dwp");
+			String response = "";
+			for (String url : urls) {
+				DefaultHttpClient client = new DefaultHttpClient();
+				HttpGet httpGet = new HttpGet(url);
+				try {
+					HttpResponse execute = client.execute(httpGet);
+					InputStream content = execute.getEntity().getContent();
+
+					BufferedReader buffer = new BufferedReader(
+							new InputStreamReader(content));
+					String s = "";
+					while ((s = buffer.readLine()) != null) {
+						response += s;
+					}
+
+				} catch (Exception e) {
+					e.printStackTrace();
+					Log.d("DEBUG", "Exception");
+				}
+			}
+			Log.d("DEBUG", "Done dwp");
+			return response;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			Log.d("DEBUG", "onPostExecute"+result);
+			if(result.equals("1"))
+			{
+				Toast.makeText(context, "You have successfully registered. Check your smail for the password.", Toast.LENGTH_LONG).show();
+			}
+			else if(result.equals("2"))
+			{
+				Toast.makeText(context,  "You have already registered. Check your smail for the password.", Toast.LENGTH_LONG).show();
+				// Calling finish will take you back to admin.java
+			}
+			else
+			{
+				Toast.makeText(context,  "The website cannot be reached", Toast.LENGTH_LONG).show();
+			}
+			finish();
+		}
+	}
+	private class DownloadWebPageTask1 extends AsyncTask<String, Void, String> {
+		@Override
+		protected String doInBackground(String... urls) {
+			Log.d("DEBUG", "Inside dwp");
+			String response = "";
+			for (String url : urls) {
+				DefaultHttpClient client = new DefaultHttpClient();
+				HttpGet httpGet = new HttpGet(url);
+				try {
+					HttpResponse execute = client.execute(httpGet);
+					InputStream content = execute.getEntity().getContent();
+
+					BufferedReader buffer = new BufferedReader(
+							new InputStreamReader(content));
+					String s = "";
+					while ((s = buffer.readLine()) != null) {
+						response += s;
+					}
+
+				} catch (Exception e) {
+					e.printStackTrace();
+					Log.d("DEBUG", "Exception");
+				}
+			}
+			Log.d("DEBUG", "Done dwp");
+			return response;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			Log.d("DEBUG", "onPostExecute"+result);
+			if(result.equals("1"))
+			{
+				set.updateset(Name, ID, Timer,dh,URL,Pass);
+				Toast.makeText(getApplicationContext(), "Settings updated", Toast.LENGTH_SHORT).show();
+				Intent i=new Intent(getApplicationContext(), Admin.class);
+				startActivity(i);
+				finish();
+			}
+			else if(result.equals("2"))
+			{
+				Toast.makeText(context,  "Your Roll Number and Password do not match. Please check your password.", Toast.LENGTH_LONG).show();
+				// Calling finish will take you back to admin.java
+			}
+			else
+			{
+				Toast.makeText(context,  "The website cannot be reached", Toast.LENGTH_LONG).show();
+			}
+			finish();
+		}
 	}
 }
